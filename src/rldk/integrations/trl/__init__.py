d7cdee019689d41fe43108f6b087fe8383bc2ec9
adityachallapally <avasanthc@gmail.com>
2025-09-22 17:04:28 -0700

    Add GRPO config CPU fallbacks

--- src/rldk/integrations/trl/__init__.py
+++ src/rldk/integrations/trl/__init__.py
@@ -5,6 +5,7 @@ from .dashboard import RLDKDashboard
 from .monitors import CheckpointMetrics, CheckpointMonitor, PPOMetrics, PPOMonitor
 from .utils import (
     check_trl_compatibility,
+    create_grpo_config,
     create_ppo_trainer,
     fix_generation_config,
     prepare_models_for_ppo,
@@ -20,6 +21,7 @@ __all__ = [
     "CheckpointMonitor",
     "CheckpointMetrics",
     "RLDKDashboard",
+    "create_grpo_config",
     "create_ppo_trainer",
     "fix_generation_config",
     "prepare_models_for_ppo",
